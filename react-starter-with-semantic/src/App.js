a1811525b05fe03568249bd11440e3d37ba52004
dangkhoaicao <dangkhoaicao@gmail.com>
2018-06-11 14:52:50 +0700

    install semantic ui react package

--- react-starter-with-semantic/src/App.js
+++ react-starter-with-semantic/src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Menu} from 'semantic-ui-react';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Menu inverted>
+        <Menu.Item active={true} name='Test'></Menu.Item>
+        <Menu.Item name='Go go go'></Menu.Item>
+      </Menu>
       </div>
     );
   }
