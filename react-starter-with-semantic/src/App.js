41e8313540dca5c1fee59dc43a0b45137c625154
dangkhoaicao <dangkhoaicao@gmail.com>
2018-07-09 11:42:52 +0700

    responsive navbar

--- react-starter-with-semantic/src/App.js
+++ react-starter-with-semantic/src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar';
+import { Image } from 'semantic-ui-react';
+
+const leftItems = [
+  { as: "a", content: "Home", key: "home" },
+  { as: "a", content: "Users", key: "users" }
+]
+
+const rightItems = [
+  { content: <Image src='https://image.flaticon.com/icons/svg/145/145859.svg' onClick={() => { console.log('ssss') }} avatar />, key: 'avatar' }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar logo={"https://react.semantic-ui.com/logo.png "}
-          leftItem={[
-            { as: "a", content: "Home", key: "home" },
-            { as: "a", content: "Users", key: "users" }
-          ]} />
+          leftItems={leftItems} rightItems={rightItems}>
+          <span>App Content</span>
+        </NavBar>
       </div>
     );
   }
