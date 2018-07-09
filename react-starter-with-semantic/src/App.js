428a0c837e2596c41602ab78fe3618667171dcfb
dangkhoaicao <dangkhoaicao@gmail.com>
2018-07-09 16:55:10 +0700

    Login Page UI

--- react-starter-with-semantic/src/App.js
+++ react-starter-with-semantic/src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import Login from './containers/AuthPages/Login';
 
-import NavBar from './components/NavBar';
-import { Image, Button } from 'semantic-ui-react';
+// import NavBar from './components/NavBar';
+// import { Image, Button } from 'semantic-ui-react';
 
-const leftItems = [
-  { as: "a", content: "Home", key: "home" },
-  { as: "a", content: "Users", key: "users" }
-]
+// const leftItems = [
+//   { as: "a", content: "Home", key: "home" },
+//   { as: "a", content: "Users", key: "users" }
+// ]
 
-const rightItems = [
-  { content: <Image src='https://image.flaticon.com/icons/svg/145/145859.svg' onClick={() => { console.log('ssss') }} avatar />, key: 'avatar' },
-  { content: <Button>Log out</Button>, key: 'logout' }
-]
+// const rightItems = [
+//   { content: <Image src='https://image.flaticon.com/icons/svg/145/145859.svg' onClick={() => { console.log('ssss') }} avatar />, key: 'avatar' },
+//   { content: <Button>Log out</Button>, key: 'logout' }
+// ]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar logo={"https://react.semantic-ui.com/logo.png "}
-          leftItems={leftItems} rightItems={rightItems}>
-          <span>App Content</span>
-        </NavBar>
-      </div>
+      <Login />
+      // <div className="App">
+      //   <NavBar logo={"https://react.semantic-ui.com/logo.png "}
+      //     leftItems={leftItems} rightItems={rightItems}>
+      //     <span>App Content</span>
+      //   </NavBar>
+      // </div>
     );
   }
 }
---
+++ react-starter-with-semantic/src/containers/AuthPages/Login.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Grid, GridRow, GridColumn, Form, Icon, Header, Segment, Container } from 'semantic-ui-react';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <Grid centered inverted>
+                <GridColumn computer={4} tablet={10} mobile={16}  >
+                    <GridRow centered>
+                        <br /><Header as='h1' textAlign='center'>Log In</Header><br />
+                    </GridRow>
+                    <GridRow centered>
+                        <Form as={Segment} textAlign='center' >
+                            <Form.Input icon='at' iconPosition='left' placeholder='Email Address' fluid />
+                            <Form.Input icon='lock' iconPosition='left' placeholder='Password' type='password' fluid />
+                            <Form.Button fluid color='green'>Log In</Form.Button>
+                            <Form.Button fluid color='facebook'><Icon name='facebook' position='left' />Log In with FaceBook</Form.Button>
+                            <Form.Button fluid color='google plus'><Icon name='google plus' />Log In with Google+</Form.Button>
+                            <a href='/#'>Forgot password?</a>
+                        </Form>
+
+                    </GridRow>
+                    <GridRow centered>
+                        <br />
+                        <Header textAlign='center' as='h4'>Dont' have an account?<a href='/#'> Sign up</a></Header>
+                    </GridRow>
+                </GridColumn>
+            </Grid>
+        )
+    }
+}
