6469a50a61c475155c0d0961a5d626fe5230e54c
dangkhoaicao <dangkhoaicao@gmail.com>
2018-07-11 12:00:09 +0700

    env management

--- react-starter-with-semantic/src/App.js
+++ react-starter-with-semantic/src/App.js
@@ -2,29 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Login from './containers/AuthPages/Login';
 
-// import NavBar from './components/NavBar';
-// import { Image, Button } from 'semantic-ui-react';
-
-// const leftItems = [
-//   { as: "a", content: "Home", key: "home" },
-//   { as: "a", content: "Users", key: "users" }
-// ]
-
-// const rightItems = [
-//   { content: <Image src='https://image.flaticon.com/icons/svg/145/145859.svg' onClick={() => { console.log('ssss') }} avatar />, key: 'avatar' },
-//   { content: <Button>Log out</Button>, key: 'logout' }
-// ]
+import config from './config'
+console.log(config);
 
 class App extends Component {
   render() {
     return (
       <Login />
-      // <div className="App">
-      //   <NavBar logo={"https://react.semantic-ui.com/logo.png "}
-      //     leftItems={leftItems} rightItems={rightItems}>
-      //     <span>App Content</span>
-      //   </NavBar>
-      // </div>
     );
   }
 }
